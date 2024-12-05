a84b56ed5099ae30757162bde8bc3e223b72c538
Pavlo Pavlenko <alskarf@gmail.com>
2024-12-05 14:01:23 +0200

    AOC: Day-5 done

--- index.js
+++ index.js
@@ -2,8 +2,10 @@ import { executeDay1 } from "./src/day-1.js";
 import { executeDay2 } from "./src/day-2.js";
 import { executeDay3 } from "./src/day-3.js";
 import { executeDay4 } from "./src/day-4.js";
+import { executeDay5 } from "./src/day-5.js";
 
 // executeDay1();
 // executeDay2();
 // executeDay3();
-executeDay4();
+// executeDay4();
+executeDay5();
