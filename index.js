d8ae0f576730504244278108bab9de6e5a39c423
Pavlo Pavlenko <alskarf@gmail.com>
2024-12-09 12:54:31 +0200

    AOC: Day-9 done

--- index.js
+++ index.js
@@ -5,7 +5,8 @@ import { executeDay4 } from "./src/day-4.js";
 import { executeDay5 } from "./src/day-5.js";
 import { executeDay6 } from "./src/day-6.js";
 import { executeDay7 } from "./src/day-7.js";
-import { executeDay8 } from "./src/day-8.js";
+// import { executeDay8 } from "./src/day-8.js";
+import { executeDay9 } from "./src/day-9.js";
 
 // executeDay1();
 // executeDay2();
@@ -14,4 +15,5 @@ import { executeDay8 } from "./src/day-8.js";
 // executeDay5();
 // executeDay6();
 // executeDay7();
-executeDay8();
+// executeDay8();
+executeDay9();
